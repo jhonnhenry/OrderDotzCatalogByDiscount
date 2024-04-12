61c931da6a27934aa680e077ffefb1e79b4380ad
Jhonatas Lima <jhlmota@gmail.com>
2024-04-12 12:11:39 -0300

    Add project

---
+++ Models.cs
@@ -0,0 +1,39 @@
+﻿
+namespace OrderDotzCatalogByDiscount
+{
+    public class Rootobject
+    {
+        public Products products { get; set; }
+    }
+
+    public class Navigation
+    {
+        public object tags { get; set; }
+        public object minPoints { get; set; }
+        public object maxPoints { get; set; }
+    }
+
+    public class Products
+    {
+        public int _pageSize { get; set; }
+        public int _page { get; set; }
+        public int _total { get; set; }
+        public Item[] items { get; set; }
+    }
+
+    public class Item
+    {
+        public string? sku { get; set; }
+        public string? shortName { get; set; }
+        public string? name { get; set; }
+        public float originalPoints { get; set; }
+        public float points { get; set; }
+        public float discountRate { get; set; }
+        public string? supplier { get; set; }
+        public float price { get; set; }
+        public string? ean { get; set; }
+        public int availableTotal { get; set; }
+        public int stockTotal { get; set; }
+        public bool isOutlet { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using OrderDotzCatalogByDiscount;
+using System.Text.Json;
+using System.Globalization;
+using System.Text;
+
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("https://api.dotz.com.br/rewards/api/default/");
+var lista = new List<Item>();
+int lastPage = 2;
+int pageSize = 100;
+
+for (int i = 1; i < lastPage; i++)
+{
+    try
+    {
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"products?_page={i}&_pageSize={pageSize}");
+        var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+        string responsejson = await httpResponseMessage.Content.ReadAsStringAsync();
+        if (httpResponseMessage.IsSuccessStatusCode)
+        {
+            var result = JsonSerializer.Deserialize<Rootobject>(responsejson);
+
+            if (result is not null)
+            {
+                lastPage = result.products._total / pageSize;
+                Console.WriteLine($" Reading {lista.Count} of {result.products._total} products.");
+                foreach (var item in result.products.items)
+                {
+                    lista.Add(item);
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message.ToString());
+        continue;
+    }
+}
+
+CsvConfiguration _configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    Delimiter = ";",
+    HasHeaderRecord = true,
+    Encoding = Encoding.UTF8,
+    IgnoreBlankLines = true
+};
+
+lista = lista.OrderByDescending(o => o.discountRate).ToList();
+
+var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+var currentDirectory = Directory.GetParent(baseDirectory)?.Parent?.Parent?.Parent;
+using (var writer = new StreamWriter($"{currentDirectory?.FullName}\\file.csv", false, Encoding.UTF8))
+using (var csv = new CsvWriter(writer, _configuration))
+{
+    csv.WriteRecords(lista);
+}
+
+Console.WriteLine("The end...");
+
